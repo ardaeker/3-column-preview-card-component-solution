a8b9bccd6f5e1b3c0df13c5dafd6882f72da2e43
Arda Eker <ardaekertr@gmail.com>
2023-12-07 11:05:03 +0300

    Add Link component to Card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Card({ ...props }: Cars) {
   return (
@@ -10,20 +11,21 @@ export default function Card({ ...props }: Cars) {
       )}
     >
       <Image src={props.content.icon} alt={props.content.title} />
-      <p className="font-bigShoulder text-card-heading mt-[2.1875rem] uppercase text-[#F2F2F2]">
+      <p className="mt-[2.1875rem] font-bigShoulder text-card-heading uppercase text-[#F2F2F2]">
         {props.content.title}
       </p>
-      <p className="font-lexend text-card-description mt-[1.5625rem] text-white opacity-75">
+      <p className="mt-[1.5625rem] font-lexend text-card-description text-white opacity-75">
         {props.content.description}
       </p>
-      <button
+      <Link
+        href="#"
         className={cn(
-          "text-card-button font-lexend mt-[1.5625rem] h-12 w-[9.125rem] rounded-[1.5625rem] border-[.125rem] border-transparent bg-[#F2F2F2] transition-colors hover:border-white hover:bg-transparent hover:text-white lg:mt-[5.1875rem]",
+          "mt-[1.5625rem] flex h-12 w-[9.125rem] items-center justify-center rounded-[1.5625rem] border-[.125rem] border-transparent bg-[#F2F2F2] font-lexend text-card-button transition-colors hover:border-white hover:bg-transparent hover:text-white lg:mt-[5.1875rem]",
           props.colors.text,
         )}
       >
         Learn More
-      </button>
+      </Link>
     </div>
   );
 }
